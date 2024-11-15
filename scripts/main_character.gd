f2c54f8411c28c071862aa25577c0a251ad59b10
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-11-15 09:51:50 +0300

    implemented the timer, timer UI, blur and de blur feature on timeout

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -5,6 +5,14 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var cant_move =false
 
+@export var originalPoint : Node3D
+
+
+
+func _ready():
+	pass
+	
+
 func chng_interaction_btn_state(state : bool):
 	$Control.visible=state
 	
@@ -12,6 +20,8 @@ func chng_interaction_btn_state(state : bool):
 func _physics_process(delta):
 	if(cant_move):
 		return
+		
+	update_timer()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * 0
@@ -32,3 +42,34 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func disable_glow():
+	$cam.environment.glow_enabled =false
+	$cam.environment.glow_intensity=0
+	$Timer.start()
+	
+	
+func de_glow():
+	var tween = get_tree().create_tween()
+	tween.finished.connect(disable_glow)
+	tween.tween_property($cam.environment,"glow_intensity",0,3)	
+	
+func _on_timer_timeout():
+	#make the screen blur and de blur in 2 seconds
+	var tween = get_tree().create_tween()
+	$cam.environment.glow_enabled =true
+	tween.finished.connect(de_glow)
+	tween.tween_property($cam.environment,"glow_intensity",5,3)
+	#send the user back next to the door
+	#position = originalPoint.position
+
+	#reset the timer and start it again
+	#$Timer.start()
+
+	pass # Replace with function body.
+	
+	
+func update_timer():
+	
+	$Control/RichTextLabel.text = str(int($Timer.time_left/60))+" : " + str( int($Timer.time_left)%60)
