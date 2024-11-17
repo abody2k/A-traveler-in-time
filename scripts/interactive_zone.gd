1c9b26c211e9ef0af7e35b3fafc3f10b929004f5
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-11-17 17:42:26 +0300

    didn't finish the game, but this is all

--- scripts/interactive_zone.gd
+++ scripts/interactive_zone.gd
@@ -16,9 +16,41 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	if(player_is_inside and Input.is_action_just_pressed("interact")):
+	if Input.is_key_pressed(KEY_N) and player_is_inside:
+		match typeOfInteraction:
 
+			TypesOfInteractions.ROUTER:
+				get_parent().get_node("MainCharacter").show_dialog("I should be go back home and learn the lesson rather than falling into the same mistake again before the time is up, I don't regret it anymore")
+				$router.visible=false
+				
+				#get_tree().current_scene
+				pass
+		pass
+	if Input.is_key_pressed(KEY_Y) and player_is_inside:
+		match typeOfInteraction:
+
+			TypesOfInteractions.ROUTER:
+				var tween = get_tree().create_tween()
+				tween.finished.connect(
+				func () :
+					$Control2.visible = true
+					$Timer.start())
+				tween.tween_method(func (x) : $Control2/Panel.modulate=Color(0,0,0,x),0.0,1.0,3)
+				#tween.tween_property($Control2/Panel,"modulate",Color(0,0,0,1),1)
+				
+				
+
+				
+				
+				#get_tree().current_scene
+				pass
+		pass
+	pass
+	if(player_is_inside and Input.is_action_just_pressed("interact")):
+		
+		if typeOfInteraction == TypesOfInteractions.ROUTER:
+			$router.visible=true
+			return
 		if $Control.visible:
 			
 			return
@@ -29,16 +61,7 @@ func _process(delta):
 		#$Control.set_process(true)
 		
 		
-		match typeOfInteraction:
 
-			TypesOfInteractions.ROUTER:
-
-				
-				
-				#get_tree().current_scene
-				pass
-		pass
-	pass
 
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
@@ -77,3 +100,21 @@ func _on_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
 	#if it's the mc hide the interaction UI
 	
 	pass # Replace with function body.
+
+
+func _on_button_pressed():
+	#erase them from your mind
+	get_tree().reload_current_scene()
+	#go back to your original place, and reset the game
+	pass # Replace with function body.
+
+
+func _on_button_2_button_down():
+	get_parent().get_node("MainCharacter").show_dialog("I should be go back home and learn the lesson rather than falling into the same mistake again, I don't regret it anymore")
+	get_parent().get_node("Magical_Door8").visible=true
+	$router.visible=false
+	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
